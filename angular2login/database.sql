1803971388485cb41494c89723fc9522a0b6e27d
PONSIVA7 <74648703+PONSIVA7@users.noreply.github.com>
2021-05-25 04:33:30 -0700

    Add files via upload

---
+++ angular2login/database.sql
@@ -0,0 +1,37 @@
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
---
+++ angular2login/database_connection.php
@@ -0,0 +1,8 @@
+
+<?php
+
+//database_connection.php
+
+$connect = new PDO("mysql:host=localhost;dbname=testing", "root", "");
+
+?>
